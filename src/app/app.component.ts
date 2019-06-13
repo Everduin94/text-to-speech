e107d6be27ef8c3c789763cfe14b719232996b5c
everduin <erik.verduin@nisc.coop>
2019-06-13 14:52:25 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SynthesisService } from './services/synthesis.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'text-to-speech';
+
+  constructor(public ss: SynthesisService) {
+      ss.updateMessage(`This actually has pretty good sound quality, I'm kind of surprised.`);
+  }
+
+  ngOnInit() {
+
+  }
 }
